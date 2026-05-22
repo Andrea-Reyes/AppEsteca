ec03c561a9855de2a623dbdc99a96496f048b4f2
Karen <karen.reyeslem@gmail.com>
2026-05-22 16:05:20 -0600

    Adding server.py

--- backend/server.py
+++ backend/server.py
@@ -307,9 +307,8 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://app-esteca-zx9t.vercel.app"], 
+    allow_origins=["https://app-esteca-zx9t.vercel.app"],
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
